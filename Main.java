0752d6226d89ee3363f22c9ee0f87b2d71da137b
PeterRanario <peterranario11@gmail.com>
2024-10-04 11:18:08 +0800

    Added Static linkedlist and scanner to accept user input

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     
-    
+    //static variables so it can be accessible within this class.
+        public static LinkedList list = new LinkedList();
+        public static Scanner sc = new Scanner(System.in);
+
 }
