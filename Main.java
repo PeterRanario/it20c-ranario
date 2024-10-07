011f4f1016c7917c90db9bc220e6e7860e94bf27
PeterRanario <peterranario11@gmail.com>
2024-10-07 12:03:44 +0800

    Initial commit on linkedlist Int

--- Main.java
+++ Main.java
@@ -2,106 +2,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    //static variables so it can be accessible within this class.
-    public static LinkedList list = new LinkedList();
-    public static Scanner sc = new Scanner(System.in);
+    public static Main(String[] args) {
 
-        //Block of methods
-        public static void adding() {
-            boolean repeat = true;
-    
-            while (repeat) {
-                System.out.println("Please input an element to add.");
-                System.out.print("Element: ");
-                String elemt = sc.next();
-                list.add(elemt);
-                System.out.println("Element ' " + elemt + " ' successfuly added.");
-                System.out.println("Do you want to add more?" + "\nPress 1 for YES \nPress 2 for NO");
-                int resp = sc.nextInt();
-    
-                if (resp == 1) {
-                    repeat = true;
-                } else if (resp == 2) {
-                    repeat = false;
-                } else {
-                    System.out.println("Invalid input. Please enter 1 or 2.");
-                    sc.close();
-                    break;
-                }
-    
-            }
-    
-        }
-    
-        public static void move() {
-            System.out.println("Enter the Index that you want to swap: ");
-            int element1 = sc.nextInt();
-            System.out.println("Enter the second index you want to swap: ");
-            int element2 = sc.nextInt();
-    
-            list.moveNodePointer(element1, element2);
-            list.printList();
-        }
-    
-        public static void display() {
-            list.printList();
-        }
-    
-        public static void delete() {
-            System.out.println("What element do you want to delete?");
-            list.printList();
-            String element = sc.next();
-            list.deleteByValue(element);
-            System.out.println("Element '" + element + "' deleted successfuly");
-            System.out.print("List: ");
-            list.printList();
+        LinkedList linkedList = new LinkedList();
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println("Hello! How are you?");
+        System.out.println("-------------------");
+
+        while true {
+
+            System.out.println("\nKindly choose a number in the menu you want to do: "
+                    + "\n1. Add Items"
+                    + "\n2. Delete an Item"
+                    + "\n3. Move/Swap an Item"
+                    + "\n4. Exit");
+
+            int userChoice = scanner.nextInt();
         }
 
-        public static void main(String[] args) {
-
-            
-            //Displaying Menu Choices
-            System.out.println("Hello! This is Linkedlist program!");
-            boolean exit = false;
-            while (!exit) {
-                System.out.println();
-                System.out.println("--------------------");
-                System.out.println("1. Add Element");
-                System.out.println("2. Move or Swap Element");
-                System.out.println("3. Display");
-                System.out.println("4. Delete");
-                System.out.println("5. Exit");
-                System.out.println("--------------------");
-                System.out.println();
-            try {
-                System.out.print("Response: ");
-                int choice = sc.nextInt();
-                switch (choice) {
-                    case 1:
-                        adding();
-                        break;
-                    case 2:
-                        move();
-                        break;
-                    case 3:
-                        display();
-                        break;
-                    case 4:
-                        delete();
-                        break;
-                    case 5:
-                        exit = true;
-                        System.out.println("Goodbye! Thanks for using this program.");
-                        break;
-                    default:
-                        System.out.println("Invalid input. Please select a valid option.");
-                }
-            } catch (Exception e) {
-                System.out.println("Error..... invalid input........");
-                break;
-            }
-        }   }
-
-
-
+    }
 }
