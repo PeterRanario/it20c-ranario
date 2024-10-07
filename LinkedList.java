e2c208d4bbbf7022c65155ca0c33263d7a142267
PeterRanario <peterranario11@gmail.com>
2024-10-07 12:07:33 +0800

    Added Method to add new node and print linke list

--- LinkedList.java
+++ LinkedList.java
@@ -11,5 +11,29 @@ public class LinkedList {
         }
 
     }
-    
+
+    // Method to add a new node at the end of the list
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currentNode = head;
+        while (currentNode.next != null) {
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.print(currentNode.value + " -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
+    }
+
 }
--- Main.java
+++ Main.java
@@ -13,12 +13,54 @@ public class Main {
         while true {
 
             System.out.println("\nKindly choose a number in the menu you want to do: "
-                    + "\n1. Add Items"
-                    + "\n2. Delete an Item"
-                    + "\n3. Move/Swap an Item"
-                    + "\n4. Exit");
+                    + "\n 1. Add Items"
+                    + "\n 2. Delete an Item"
+                    + "\n 3. Move/Swap an Item"
+                    + "\n 4. Exit");
 
             int userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    
+                    System.out.println("Enter 5 elements that you want to add: ");
+                    for (int i = 0; i < 5; i++) {
+                        int element = scanner.nextInt();
+                        linkedList.add(element);
+                    }
+                    System.out.println("Current Linked List:");
+                    linkedList.printList();
+                    break;
+
+                case 2:
+                    
+                    System.out.println("Enter the value of the item you want to delete: ");
+                    int valueToDelete = scanner.nextInt();
+                    linkedList.deleteByValue(valueToDelete);
+                    System.out.println("Item deleted (if existed). Current Linked List:");
+                    linkedList.printList();
+                    break;
+
+                case 3:
+                    
+                    System.out.println("Enter the index of the item you want to move: ");
+                    int currentIndex = scanner.nextInt();
+                    System.out.println("Enter the new index to move the item to: ");
+                    int newIndex = scanner.nextInt();
+                    linkedList.moveNodePointer(currentIndex, newIndex);
+                    System.out.println("Item moved! Current Linked List:");
+                    linkedList.printList();
+                    break;
+
+                case 4:
+                    
+                    System.out.println("Exit.Goodbye!");
+                    scanner.close(); // Closing the scanner
+                    return; // Exit the program
+
+                default:
+                    System.out.println("Invalid input! Please choose a valid option.");
+            }
         }
 
     }
