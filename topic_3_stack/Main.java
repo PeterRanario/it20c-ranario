9965cf6ffc46fe63373e8f2fa327b7e40ae02f86
PeterRanaio <peterranario11@gmail.com>
2024-10-13 11:44:20 +0800

    added scanner input choice

--- topic_3_stack/Main.java
+++ topic_3_stack/Main.java
@@ -15,6 +15,26 @@ public class Main {
 
     }
 
+    while true {
+        System.out.println("Please choose what you want to do: "
+                + "0. Size"
+                + "1. Push"
+                + "2. Pop"
+                + "3. Exit");
+            int choice = sc.nextint();
 
+        switch(choice) {
+            case 1;
+
+            System.out.println("Size of stack is: " + stack.size());
+            System.out.println("---------------------------------");
+            break;
+            
+
+        }
+                
+        
+
+    }
 
 }
