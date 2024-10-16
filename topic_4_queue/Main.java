89d881a9332edc034b562554e332248a9224650a
PeterRanario <peterranario11@gmail.com>
2024-10-17 01:46:43 +0800

    added new scanner on main class

--- topic_4_queue/Main.java
+++ topic_4_queue/Main.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 
 public class Main {
 
-    
+    Scanner sc = new Scanner(System.in);
+    Queue customerQueue = new Queue();
+
+    System.out.println("Hello there, hehe!");
+             
+        while(true) {
+                               
+            System.out.println("Choose what you want:"
+            + " 1. Display Queue."
+            + " 2. Add Customer. "
+            + " 3. Serve Customer."
+            + " 4. Exit");
+        }
+        
+            
 }
